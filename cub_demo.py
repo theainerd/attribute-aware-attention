0cba75f99e1713790842c87cecf384134708bb51
Shyam <beingprofess@gmail.com>
2019-02-07 20:23:24 +0530

    data

--- cub_demo.py
+++ cub_demo.py
@@ -1,5 +1,16 @@
 ''' A3M for fine-grained recognition
 '''
+from keras import backend as K
+import os
+
+def set_keras_backend(backend):
+
+    if K.backend() != backend:
+        os.environ['KERAS_BACKEND'] = backend
+        reload(K)
+        assert K.backend() == backend
+
+set_keras_backend("theano")
 
 from __future__ import print_function
 import sys
