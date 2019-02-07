b22c6acd48470f98fc73d47931566b558ad22c75
Shyam <beingprofess@gmail.com>
2019-02-07 20:28:38 +0530

    data

--- cub_demo.py
+++ cub_demo.py
@@ -5,19 +5,8 @@ import sys
 sys.path.append("..")
 sys.setrecursionlimit(10000)
 
-
-from keras import backend as K
 import os
-
-def set_keras_backend(backend):
-
-    if K.backend() != backend:
-        os.environ['KERAS_BACKEND'] = backend
-        reload(K)
-        assert K.backend() == backend
-
-set_keras_backend("theano")
-
+os.environ['KERAS_BACKEND'] = 'theano'
 
 
 import numpy as np
