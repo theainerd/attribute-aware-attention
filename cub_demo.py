5f9c722fa02bb3711bc7b5a99a713e34928d1a61
Shyam <beingprofess@gmail.com>
2019-02-08 16:17:08 +0530

    attribute

--- cub_demo.py
+++ cub_demo.py
@@ -154,7 +154,7 @@ if(flag_test):
 
 model.summary()
 #plot(model, show_shapes=True, to_file='./fig/'+net+'_attention.png')
-
+print("worked")
 # the data, shuffled and split between train and test sets
 (X_train, y_train),(X_test, y_test),(A_train,A_test,C_A)=eval(dataset).load_data(
     data_folder, target_size=(img_rows, img_cols), bounding_box=True)
