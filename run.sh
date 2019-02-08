324ec51942c8ec32cdd464c56184daa1295b7978
Shyam <beingprofess@gmail.com>
2019-02-08 14:00:07 +0530

    attribute

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ datetime=`date -d @$timestamp +"%Y-%m-%d-%H:%M:%S"`
 net=ResNet50
 data_dir=/home/ubuntu/shyam/attribute-aware-attention/CUB_200_2011
 gpu_id=0
-THEANO_FLAGS='device=cuda0'$gpu_id',floatX=float32,lib.cnmem=0.6' python cub_demo.py $net $data_dir | tee "./log/"$net"-"$datetime".log.txt"
+THEANO_FLAGS='device=gpu'$gpu_id',floatX=float32,lib.cnmem=0.6' python cub_demo.py $net $data_dir | tee "./log/"$net"-"$datetime".log.txt"
 
 
