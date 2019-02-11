2524f0e2022586b7cd8ac247c1ae3ac1edd10e0e
Shyam <beingprofess@gmail.com>
2019-02-11 18:59:49 +0530

    add

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -1,7 +1,7 @@
 lines = open('../data/Anno/list_attr_cloth.txt', 'r').readlines()
 output = open("../data/Anno/list_attr_cloth.txt", 'w')
 
-for line in sorted(lines, key=itemgetter(0)):
+for line in sorted(lines, key=lambda line: line.split()[1]):
 	print(line)
 
 
