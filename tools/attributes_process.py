852e4c7b56998e741731dff324535dee50c4850f
Shyam <beingprofess@gmail.com>
2019-02-11 18:18:12 +0530

    add

--- tools/attributes_process.py
+++ tools/attributes_process.py
@@ -26,21 +26,21 @@ nums = a[1:]-a[:-1]+1
 print(np.sum(nums))
 print(nums.tolist())
 
-# transform binary attribute to clustered attribute
-nb_attr = len(start_idxs)-1
-A_all = np.zeros((289222,nb_attr))
-image_attribute_file = 'attributes/list_attribute_image.txt'
-f2 = open(image_attribute_file,'rb')
-for line in f2.readlines():
-	strs = re.split(' ', line)
-	img_id = int(strs[0])-1
-	attr_id = int(strs[1])
-	is_present = int(strs[2])
-	for i in range(len(start_idxs)):
-		if(attr_id<start_idxs[i]):
-			break
-		A_all[img_id][i-1] = attr_id-start_idxs[i-1]+1 # 0 mean no attr
-print(A_all[1])
+# # transform binary attribute to clustered attribute
+# nb_attr = len(start_idxs)-1
+# A_all = np.zeros((289222,nb_attr))
+# image_attribute_file = 'attributes/list_attribute_image.txt'
+# f2 = open(image_attribute_file,'rb')
+# for line in f2.readlines():
+# 	strs = re.split(' ', line)
+# 	img_id = int(strs[0])-1
+# 	attr_id = int(strs[1])
+# 	is_present = int(strs[2])
+# 	for i in range(len(start_idxs)):
+# 		if(attr_id<start_idxs[i]):
+# 			break
+# 		A_all[img_id][i-1] = attr_id-start_idxs[i-1]+1 # 0 mean no attr
+# print(A_all[1])
 
 # new_attr_file = 'processed_attributes.txt'
 # np.savetxt(new_attr_file,A_all,fmt='%d')
