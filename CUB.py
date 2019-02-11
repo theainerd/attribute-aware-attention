09f5fa344ad4a72ef7845cee46066f79d5c5ae14
Shyam <beingprofess@gmail.com>
2019-02-11 12:07:17 +0530

    add attributes

--- CUB.py
+++ CUB.py
@@ -128,7 +128,7 @@ test_idx = []
 i=0
 for line in split_rf.readlines():
     strs = line.strip().split(' ')
-    print(strs.shape())
+    print(len(strs))
     train_test_list.append(strs[12])
     if(strs[12]=='train'):
         train_idx.append(i)
