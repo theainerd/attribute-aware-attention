afd0687b9d7c894dc76ef538445db767cea7889d
Shyam <beingprofess@gmail.com>
2019-02-11 13:54:21 +0530

    add

--- CUB.py
+++ CUB.py
@@ -168,6 +168,7 @@ images_rf = open(images_file,'r')
 for line in images_rf.readlines():
     strs = line.strip().split(' ')
     img = image.load_img(data_folder+ strs[0])
+    print(bb_list[int(strs[0])-1])
     if(bounding_box):
         img = img.crop(bb_list[int(strs[0])-1])
     img = img.resize(target_size)
