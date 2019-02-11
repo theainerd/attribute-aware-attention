0d146df194d52c31fdc6d4bdb9b619ce9176d2fc
Shyam <beingprofess@gmail.com>
2019-02-11 16:48:37 +0530

    add

--- tools/attributes_process.py
+++ tools/attributes_process.py
@@ -8,7 +8,7 @@ attribute_name_file = '../data/Anno/list_attr_cloth.txt'
 f1 = open(attribute_name_file, 'rb')
 start_idxs = []
 last_attr = ''
-i = 
+i = 0
 for line in f1.readlines():
 	line = line.decode("utf-8")
 	while line.replace("   ", "  ") != line:
