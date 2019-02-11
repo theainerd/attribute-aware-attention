4d026105cefa0958dc19792c3c0004cb703e2ec9
Shyam <beingprofess@gmail.com>
2019-02-11 21:20:13 +0530

    add

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -2,4 +2,5 @@ attribute_name_file = '../data/Anno/list_attr_cloth.txt'
 f1 = open(attribute_name_file, 'rb')
 f = open("list_attr_cloth_new.txt",'wb')
 for line in f1.readlines():
+	print(line)
 	f.writelines(line)
