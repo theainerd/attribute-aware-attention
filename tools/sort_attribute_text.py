8359f7cd9f4f7d6a2c773ff25bf8169150ec5310
Shyam <beingprofess@gmail.com>
2019-02-12 11:36:08 +0530

    ds

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -6,7 +6,6 @@ import numpy as np
 attribute_name_file = '../data/Anno/list_attr_cloth.txt'
 f1 = open(attribute_name_file, 'rb')
 f = open("list_attr_cloth_new.txt",'wb')
-i = 0
 for line in f1.readlines():
 	print(line)
 	f.writelines(line)
