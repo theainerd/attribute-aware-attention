a19dc15d667f637e19980903eb3aed303d589e46
Shyam <beingprofess@gmail.com>
2019-02-12 17:10:34 +0530

    ds

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -5,10 +5,12 @@ import numpy as np
 
 attribute_name_file = '../data/Anno/list_attr_cloth.txt'
 f1 = open(attribute_name_file, 'rb')
-f = open("list_attr_cloth.txt",'wb')
+f = open("list_attr_cloth_new.txt",'wb')
 i = 0
 for line in f1.readlines():
 	line = line.decode("utf-8")
-	line = str.encode(line)
+	while line.replace('   ','  ') != line:
+		line = line.replace('   ', '  ')
+		line = line.replace('   ', ' ')
 	f.write(line)
 	i = i+1
