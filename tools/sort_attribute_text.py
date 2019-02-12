05267b7933b31a900a6e4e189ba2d1e94ca2312c
Shyam <beingprofess@gmail.com>
2019-02-12 17:35:19 +0530

    ds

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -9,7 +9,7 @@ f = open("list_attr_cloth_new.txt",'wb')
 i = 0
 for line in f1.readlines():
 	line = line.decode("utf-8")
-	while line.replace('   ',' ') != line:
+	while line.replace('  ',' ') != line:
 		line = line.replace('   ', '  ')
 		line = line.replace('  ', ' ')
 		line = line.replace('  ', ' ')
