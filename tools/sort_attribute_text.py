d4cf4c28e4376b371d8eb2538198cf68fa93a60e
Shyam <beingprofess@gmail.com>
2019-02-12 17:42:38 +0530

    ds

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -10,10 +10,10 @@ i = 0
 for line in f1.readlines():
 	line = line.decode("utf-8")
 	while line.replace('  ',' ') != line:
-		line = line.replace('   ', ' ')
-		line = line.replace('  ', ' ')
-		line = line.replace('  ', ' ')
-	print(line)
+		line = line.replace('   ',' ')
+		line = line.replace('  ',' ')
+		line = line.replace('  ',' ')
+	print(line.strip().split(' '))
 	line = str.encode(line)	
 	f.write(line)
 	i = i+1
