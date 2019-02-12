80c5e1ae0c2636a2b7dc8e75a7426d841fab4135
Shyam <beingprofess@gmail.com>
2019-02-12 18:42:31 +0530

    attention

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 
-data = pd.read_csv("list_attr_cloth_new.txt",sep = " ",error_bad_lines=False)
+data = pd.read_csv("list_attr_cloth_new.txt",sep = " ",error_bad_lines=False,header=None)
+data.columns = ["attr_subclass",'attr_class']
 print(data[:4])
 
