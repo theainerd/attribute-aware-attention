3d527d1c8ba6cf8ad69faa84ed76bb25dbe246ce
Shyam <beingprofess@gmail.com>
2019-02-12 19:01:57 +0530

    attention

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -7,3 +7,4 @@ print(data[:4])
 data1 = data.sort_values(by='attr_class', ascending=True)
 data1 = data1[["attr_class","attr_subclass"]]
 print(data1[:4])
+data1.to_csv(sep=' ', index=False, header=False)
