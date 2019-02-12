0884614cc41e7fbdb62c3d410a7f90b521c36646
Shyam <beingprofess@gmail.com>
2019-02-12 18:38:32 +0530

    attention

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -5,7 +5,7 @@ import numpy as np
 
 attribute_name_file = 'list_attr_cloth.txt'
 f1 = open(attribute_name_file, 'rb')
-f = open("list_attr_cloth_new.txt",'wb')
+f = open("list_attr_cloth_new.txt",'w')
 i = 0
 for line in f1.readlines():
 	line = line.decode("utf-8")
