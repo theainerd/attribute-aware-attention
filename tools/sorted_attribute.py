b82c8ae4dd6a47f63e8911a57b025bc89985fd5d
Shyam <beingprofess@gmail.com>
2019-02-12 18:52:18 +0530

    attention

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -5,6 +5,6 @@ data.columns = ["attr_subclass",'attr_class']
 print(data[:4])
 
 data1 = data.sort_values(by='attr_class', ascending=True)
-# data1 = data1['attr_class','attr_subclass']
+data1 = data1['attr_class','attr_subclass']
 print(data1[:20])
 print(data1[900:950])
