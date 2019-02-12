916e215d5fc7994432a6ced01cf9bfea51bffd22
Shyam <beingprofess@gmail.com>
2019-02-12 18:44:40 +0530

    attention

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -4,5 +4,5 @@ data = pd.read_csv("list_attr_cloth_new.txt",sep = " ",error_bad_lines=False,hea
 data.columns = ["attr_subclass",'attr_class']
 print(data[:4])
 
-data.sort_values(by='attr_class', ascending=False)
-
+data1 = data.sort_values(by='attr_class', ascending=False)
+print(data1)
