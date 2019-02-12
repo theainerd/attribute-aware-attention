273e0b305378a70b16f91a298abab53b8c7d4580
Shyam <beingprofess@gmail.com>
2019-02-12 13:57:08 +0530

    ds

--- tools/attributes_process.py
+++ tools/attributes_process.py
@@ -16,6 +16,7 @@ for line in f1.readlines():
 	while line.replace('   ','  ') != line:
 		line = line.replace('   ', '  ')
 	strs = re.split('  ', line)
+	print(strs)
 	if(strs[0]!=last_attr):
 		start_idxs.append(int(strs[1]))
 	last_attr = strs[0]
