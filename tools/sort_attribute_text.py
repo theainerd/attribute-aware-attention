bf9e8a10fca6bac2f9e0f986dd2eac0c0b7ff0b7
Shyam <beingprofess@gmail.com>
2019-02-12 11:48:57 +0530

    ds

--- tools/sort_attribute_text.py
+++ tools/sort_attribute_text.py
@@ -3,9 +3,9 @@
 import re
 import numpy as np
 
-attribute_name_file = '../data/Anno/list_attr_cloth.txt'
+attribute_name_file = '../data/Anno/list_category_img.txt'
 f1 = open(attribute_name_file, 'rb')
-f = open("list_attr_cloth_new.txt",'wb')
+f = open("list_category_img_small.txt",'wb')
 i = 0
 for line in f1.readlines():
 	if i < 10000:
