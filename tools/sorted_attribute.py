0ff4571ef92bf800793a1d8a89ec0304965fd579
Shyam <beingprofess@gmail.com>
2019-02-12 19:04:02 +0530

    attention

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -7,4 +7,4 @@ print(data[:4])
 data1 = data.sort_values(by='attr_class', ascending=True)
 data1 = data1[["attr_class","attr_subclass"]]
 print(data1[:4])
-data1.to_csv(sep=' ', index=False, header=False)
+data1.to_csv('list_attr_cloth_new_sorted.txt',sep=' ', index=False, header=False)
