3246421e7a4e6110a5efab41ad64f5981e666e61
Shyam <beingprofess@gmail.com>
2019-02-12 19:16:00 +0530

    attention

--- tools/attributes_process.py
+++ tools/attributes_process.py
@@ -34,6 +34,7 @@ start_idxs = []
 last_attr = ''
 for line in f1.readlines():
 	strs = re.split(' ', line)
+	print(strs)
 	if(strs[1]!=last_attr):
 		start_idxs.append(int(strs[0]))
 	last_attr = strs[1]
