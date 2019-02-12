5018afd49fa2f8be12a42913439b3efc21fbbd84
Shyam <beingprofess@gmail.com>
2019-02-12 17:33:22 +0530

    ds

--- tools/sorted_attribute.py
+++ tools/sorted_attribute.py
@@ -1,14 +1,4 @@
-#coding=utf-8
+import pandas as pd 
 
-import re
-import numpy as np
-
-attribute_name_file = '../data/Anno/list_category_img_small.txt'
-f1 = open(attribute_name_file, 'rb')
-i = 0
-for line in f1.readlines():
-	if i < 10000:
-		f.write(line)
-		i = i+1
-	else:
-		break
+data = pd.read_csv("list_attr_cloth_new.txt",sep = " ")
+print(data[:4])
